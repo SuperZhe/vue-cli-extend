fb94dc41d80cdafd119bb74722dada6dbb6c6b3c
王圣哲 <siwenMail@163.com>
2019-12-20 17:48:10 +0800

    添加错误校验

--- src/main.js
+++ src/main.js
@@ -1,33 +1,36 @@
-import Vue from "vue";
+import Vue from 'vue'
 
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import Element from "element-ui";
-import "element-ui/lib/theme-chalk/reset.css";
-import "element-ui/lib/theme-chalk/index.css";
-Vue.config.productionTip = false;
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/reset.css'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.config.productionTip = false
 
-Vue.use(Element);
+Vue.use(Element)
 // 注册接口插件使用
-import api from "./server";
-Vue.use(api);
-console.log(1111)
-console.log(1111)
-console.log(1111)
+import api from './server'
+Vue.use(api)
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 
+Sentry.init({
+  dsn: 'https://b7327efacd46@sentry.io/1861773',
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+})
 // 全局方法
-import lib from "./lib";
-Vue.use(lib);
+import lib from './lib'
+Vue.use(lib)
 
 // 全局注册过滤器
-import filters from "./filters";
+import filters from './filters'
 Object.keys(filters).forEach(val => {
-  Vue.filter(val, filters[val]);
-});
+  Vue.filter(val, filters[val])
+})
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
